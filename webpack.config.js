a760f7a75631d2e8f91fac9e86b1ce1c06829519
Minghao, Li <leemiyinghao@gmx.com>
2020-12-14 21:31:22 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+};
+module.rules = [{
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: [
+    { loader: 'babel-loader' }
+  ]
+},
+{
+  test: /\.(gltf)$/,
+  use: [
+    {
+      loader: "gltf-webpack-loader"
+    }
+  ]
+}];
